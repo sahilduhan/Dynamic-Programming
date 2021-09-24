ecb36f5d9bb345e1673811f476ea9f0ddd9af680
Sahil Duhan <duhan.sahil16@gmail.com>
2021-09-24 23:28:02 +0530

    git update

--- KnapSack/problems/subset_problem.cpp
+++ KnapSack/problems/subset_problem.cpp
@@ -1,9 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int subSetSum(vector<int> arr, int target)
-{
+{   
     int finalAns=0;
-    
+    bool t[arr.size()+1][target+1];
+    for(int i=0; i<arr.size(); i++)
+    {
+        for(int j=0; j<arr.size(); j++)
+        {
+            if(i==0) t[i][j]=true;
+            if(j==0) t[i][j]=false;
+        }
+    }
+
+
+   
     return finalAns;
 }
 int main()
